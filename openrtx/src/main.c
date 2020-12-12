928b25794fb099ac681713c54b321b64ae368fd2
Silvano Seva <silseva@fastwebnet.it>
2020-12-12 10:16:26 +0100

    Made calls to 'display_renderRows' and 'display_render' blocking, thus not requiring anymore to busy wiat on 'display_renderingInProgress'. Modifications involved only MDx display driver, the others were already blocking.

--- openrtx/src/main.c
+++ openrtx/src/main.c
@@ -39,7 +39,8 @@
 #include <hwconfig.h>
 
 /* If battery level is below 0% draw low battery screen, wait and shutdown */
-void check_battery() {
+void check_battery()
+{
     OS_ERR os_err;
 
     // Check battery percentage
@@ -47,7 +48,8 @@ void check_battery() {
     float charge = battery_getCharge(vbat);
 
     // Draw low battery screen
-    if (charge <= 0) {
+    if (charge <= 0)
+    {
         ui_drawLowBatteryScreen();
         gfx_render();
 
@@ -77,7 +79,6 @@ int main(void)
     // Display splash screen
     ui_drawSplashScreen();
     gfx_render();
-    while(gfx_renderingInProgress());
 
     // Wait 30ms before turning on backlight to hide random pixels on screen
     OSTimeDlyHMSM(0u, 0u, 0u, 30u, OS_OPT_TIME_HMSM_STRICT, &os_err);
@@ -90,7 +91,8 @@ int main(void)
     create_threads();
 
     // Auxiliary functions loop
-    while(true) {
+    while(true)
+    {
         check_battery();
         OSTimeDlyHMSM(0u, 1u, 0u, 0u, OS_OPT_TIME_HMSM_STRICT, &os_err);
     }
