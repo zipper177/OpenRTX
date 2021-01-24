86732d5cb3d5d2a9e576b70e397a14f5d3eae4d2
Federico Amedeo Izzo <federico@izzo.pro>
2021-01-24 18:11:09 +0100

    CPS: Add Zone and Contact reading function stubs

--- platform/drivers/NVM/nvmem_MD3x0.c
+++ platform/drivers/NVM/nvmem_MD3x0.c
@@ -278,3 +278,14 @@ int nvm_readChannelData(channel_t *channel, uint16_t pos)
 
     return 0;
 }
+
+int nvm_readZoneData(zone_t *zone, uint16_t pos)
+{
+    return -1;
+}
+
+int nvm_readContactData(contact_t *contact, uint16_t pos)
+{
+    return -1;
+}
+
