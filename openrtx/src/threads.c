11864cadc199ddd246f90fbb462b3f4c46954858
Federico Amedeo Izzo <federico@izzo.pro>
2021-02-13 22:11:25 +0100

    Consider RTC time UTC instead of local time, applying timezone

--- openrtx/src/threads.c
+++ openrtx/src/threads.c
@@ -263,6 +263,7 @@ static void dev_task(void *arg)
 
 #ifdef HAS_RTC
         state.time = rtc_getTime();
+        state_applyTimezone();
 #endif
         state.v_bat = platform_getVbat();
         state.charge = battery_getCharge(state.v_bat);
