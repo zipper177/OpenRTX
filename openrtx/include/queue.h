8e04fec8a9ceea047647e89cd734b4136eab0795
Silvano Seva <silseva@fastwebnet.it>
2021-03-10 17:00:29 +0100

    Minimal refactoring of queue sources

--- openrtx/include/queue.h
+++ openrtx/include/queue.h
@@ -1,3 +1,36 @@
+/***************************************************************************
+ *   Copyright (C) 2020 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN                             *
+ *                         Frederik Saraci IU2NRO                          *
+ *                         Silvano Seva IU2KWO                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -17,10 +50,24 @@ typedef struct queue_t
     uint32_t buffer[MSG_QTY];
 } queue_t;
 
+/**
+ *
+ */
 void queue_init(queue_t *q);
 
+/**
+ *
+ */
 void queue_terminate(queue_t *q);
 
+/**
+ *
+ */
 bool queue_pend(queue_t *q, uint32_t *msg, bool blocking);
 
+/**
+ *
+ */
 bool queue_post(queue_t *q, uint32_t msg);
+
+#endif
