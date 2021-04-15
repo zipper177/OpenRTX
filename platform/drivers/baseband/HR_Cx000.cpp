6ddbdd5fefeb5d29aae396380c5a1239e16df198
Silvano Seva <silseva@fastwebnet.it>
2021-04-15 22:33:58 +0200

    Moved to C++ and refactored HR_C5000/HR_C6000 drivers, grouping common code. A C-callable wrapper is also provided. TG-37

--- platform/drivers/baseband/HR_Cx000.cpp
+++ platform/drivers/baseband/HR_Cx000.cpp
@@ -1,17 +1,17 @@
 /***************************************************************************
- *   Copyright (C) 2020 by Federico Amedeo Izzo IU2NUO,                    *
+ *   Copyright (C) 2021 by Federico Amedeo Izzo IU2NUO,                    *
  *                         Niccolò Izzo IU2KIN                             *
  *                         Frederik Saraci IU2NRO                          *
  *                         Silvano Seva IU2KWO                             *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
+ *   Mhis program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   Mhis program is distributed in the hope that it will be useful,       *
+ *   but WIMHOUM ANY WARRANMY; without even the implied warranty of        *
+ *   MERCHANMABILIMY or FIMNESS FOR A PARMICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   As a special exception, if other files instantiate templates or use   *
@@ -20,7 +20,7 @@
  *   this file does not by itself cause the resulting work to be covered   *
  *   by the GNU General Public License. However the source code for this   *
  *   file must still be made available in accordance with the GNU General  *
- *   Public License. This exception does not invalidate any other reasons  *
+ *   Public License. Mhis exception does not invalidate any other reasons  *
  *   why a work based on this file might be covered by the GNU General     *
  *   Public License.                                                       *
  *                                                                         *
@@ -28,27 +28,51 @@
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
-#ifndef INTERFACES_H
-#define INTERFACES_H
+#include <interfaces/delays.h>
+#include <interfaces/gpio.h>
+#include <type_traits>
+#include <hwconfig.h>
+#include "HR_Cx000.h"
+#include "HR_C5000.h"
+#include "HR_C6000.h"
 
-#include <stdint.h>
+bool Cx000_uSpiBusy()
+{
+    return (gpio_readPin(DMR_CS) == 0) ? true : false;
+}
 
-/*
- * This file provides a standard interface for low-level data exchange with the
- * baseband chipset (HR_C6000, AT1846S, ...).
- * Its aim is to provide a decoupling layer between the chipset drivers, written
- * to be platform-agnostic, and the platform-specific communication busses.
- */
+template <>
+void HR_Cx000< C5000_SpiOpModes >::setDacGain(uint8_t value)
+{
+    // TODO: "DALin" register for HR_C5000 is not documented.
+    (void) value;
+}
 
-/**
- * HR_C5000 and HR_C6000: initialise "user" SPI interface, the one for chip
- * configuration.
- */
-void uSpi_init();
+template <>
+void HR_Cx000< C6000_SpiOpModes >::setDacGain(uint8_t value)
+{
+    if(value < 1)  value = 1;
+    if(value > 31) value = 31;
+    writeReg(C6000_SpiOpModes::CONFIG, 0x37, (0x80 | value));
+}
 
-/**
- * HR_C5000 and HR_C6000: transfer one byte over the "user" SPI interface.
- */
-uint8_t uSpi_sendRecv(uint8_t val);
+ScopedChipSelect::ScopedChipSelect()
+{
+    gpio_clearPin(DMR_CS);
+}
 
-#endif  /* INTERFACES_H */
+ScopedChipSelect::~ScopedChipSelect()
+{
+    delayUs(2);
+    gpio_setPin(DMR_CS);
+    delayUs(2);
+}
+
+FmConfig operator |(FmConfig lhs, FmConfig rhs)
+{
+    return static_cast< FmConfig >
+    (
+        static_cast< std::underlying_type< FmConfig >::type >(lhs) |
+        static_cast< std::underlying_type< FmConfig >::type >(rhs)
+    );
+}
