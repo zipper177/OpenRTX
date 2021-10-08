ac5dce484559192f9bd1e3146295a4d60cd87ad6
Silvano Seva <silseva@fastwebnet.it>
2021-10-08 19:34:08 +0200

    Fixed syntax error in MD3x0 and UV3x0 radio drivers

--- platform/drivers/baseband/radio_UV3x0.cpp
+++ platform/drivers/baseband/radio_UV3x0.cpp
@@ -232,7 +232,7 @@ void radio_enableTx()
 
     gpio_setPin(PA_EN_2);
 
-    if(config.opMode == FM && config->txToneEn)
+    if(config->txToneEn)
     {
         at1846s.enableTxCtcss(config->txTone);
     }
